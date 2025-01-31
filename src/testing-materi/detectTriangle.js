12d397f19b51a9a1fd6d8e1292e3e53fba520e75
rizalsuryana <rizal.suryn@gmail.com>
2025-01-31 12:07:42 +0700

    1st scenario

--- src/testing-materi/detectTriangle.js
+++ src/testing-materi/detectTriangle.js
@@ -1,4 +1,21 @@
 const detectTriangle = (a, b, c) => {
+//   if (typeof a !== 'number') {
+//     throw new Error('Input must be a number');
+//   }
+
+  //   if (typeof b !== 'number'){
+  //     throw new Error('Input must be a number');
+  //   }
+
+  //   if (typeof c !== 'number'){
+  //     throw new Error('Input must be a number');
+  //   }
+
+  //   after passed the test let's change the code
+
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number'){
+    throw new Error('Input must be a number');
+  }
 
 };
 
--- src/testing-materi/detectTriangle.test.js
+++ src/testing-materi/detectTriangle.test.js
@@ -1,7 +1,10 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import detectTriangle from './detectTriangle';
 
 describe('detectTriangle function', ()=> {
   it('should throw error when input is not a number', ()=> {
-
+    expect(()=> detectTriangle('a', 2, 3)).toThrowError('Input must be a number');
+    expect(()=> detectTriangle(1, 'b', 3)).toThrowError('Input must be a number');
+    expect(()=> detectTriangle(1, 2, 'c')).toThrowError('Input must be a number');
   });
 });
