872921ca030154d137f9a213444950ed90c3e836
rizalsuryana <rizal.suryn@gmail.com>
2025-01-31 21:03:53 +0700

    passed

--- src/testing-materi/detectTriangle.js
+++ src/testing-materi/detectTriangle.js
@@ -13,9 +13,42 @@ const detectTriangle = (a, b, c) => {
 
   //   after passed the test let's change the code
 
+  // if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number'){
+  //   throw new Error('Input must be a number');
+  // }
+
+  // if (a <= 0 || b <= 0 || c <= 0) {
+  //   throw new Error('Input must be a positive number');
+  // }
+
+  // if (a + b <= c || a + c <= b || b + c <= a) {
+  //   throw new Error('Input is triangle inequality violation');
+  // }
+
+  // Refactor!
+  [a, b, c] = [a, b, c].sort((x, y) => x - y);
+
   if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number'){
     throw new Error('Input must be a number');
   }
+
+  if (a <= 0) {
+    throw new Error('Input must be a positive number');
+  }
+
+  if (a + b <= c){
+    throw new Error('Input is triangle inequality violation');
+  }
+
+  if (a === b && b === c){
+    return 'Segitiga Sama Sisi';
+  }
+
+  if (a===b || b===c){
+    return 'Segitiga Sama Kaki';
+  }
+
+  return 'Segitiga Sembarang';
 
 };
 
